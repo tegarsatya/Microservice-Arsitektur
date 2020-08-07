a21fa80e44c281b65f28e9d92f6f2ee592b9b347
tegar satya  negara <tegarsatyanegara.tsn@gmail.com>
2020-08-07 11:15:21 +0700

    Api Upload Image Service Media

--- service-media/app.js
+++ service-media/app.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const  cookieParser = require('cookie-parser');
-const  logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-const  indexRouter = require('./routes/index');
-const  mediaRouter = require('./routes/media');
+const indexRouter = require('./routes/index');
+const mediaRouter = require('./routes/media');
 
-const  app = express();
+const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, imit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
