ed4c74ded8001f493a4ca649791dae3180047df6
tegar satya  negara <tegarsatyanegara.tsn@gmail.com>
2020-08-10 22:31:35 +0700

    api register login and update users finish

--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const usersHandler = require('./handler/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-});
+// router
+router.post('/register', usersHandler.register);
+router.post('/login', usersHandler.login);
+router.put('/:id', usersHandler.update);
 
 module.exports = router;
