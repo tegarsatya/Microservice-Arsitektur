084b89c972e6c140e107996f1f2a4bc38ab4edcf
tegar satya  negara <tegarsatyanegara.tsn@gmail.com>
2020-08-13 23:03:51 +0700

    membuat model dan membuat api-mentros&courses

--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Perbaikan Api-Mentors By Tegar
+Route::get('mentors', 'MentorController@index');
+Route::get('mentors/{id}', 'MentorController@show');
+Route::post('mentors', 'MentorController@create');
+Route::put('mentors/{id}', 'MentorController@update');
+Route::delete('mentors/{id}', 'MentorController@destroy');
+
+
+// Api-Course
+Route::post('courses', 'CourseController@create');
