f855e5ecb00104c9898e034cb96c900249175631
Tegar Satya Negara <59832647+tegarsatya@users.noreply.github.com>
2020-10-12 22:10:55 +0700

    login form

--- service-frontend-memberpage/src/App.js
+++ service-frontend-memberpage/src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import {createBrowserHistory} from 'history';
+import {Router, Route, Switch} from 'react-router-dom';
 
 import 'assets/css/style.css';
 
+import GeusRoute from 'components/Routes/GuesRoute';
+// import MemberRoute from 'components/Routes/MemberRoute';
+import NotFound from 'pages/404';
+import Login from 'pages/Login';
+
 function App() {
+    const history = createBrowserHistory ({ basename: process.env.PUBLIC_URL })
+
+
     return ( 
-        <div className = "container mx-auto">
-            <h1 className="text-6xl"></h1>
-            <div>
- 
-            </div>
-        </div>   
+        <>
+            <Router history={history}>
+                <Switch>
+                    <GeusRoute path="/login" component={Login}></GeusRoute>
+                                   
+                    <Route path="*" component={NotFound}></Route>
+                </Switch>
+            </Router>
+        </>   
     )
 }
 
---
+++ service-frontend-memberpage/src/components/Routes/MemberRoute.js
@@ -0,0 +1,23 @@
+import React from 'module';
+import {Route, Redirect, withRouter } from 'react-router-dom';
+
+const MemberRoute = ({component: Component, match, path, location, ...rest}) => {
+
+    const ok = localStorage.getItem("BWAMICRO:token")
+    localStorage.removeItem("BWAMICRO:redirect")
+
+
+    return (
+        <Route 
+            {...rest}
+
+            render={props => 
+                ok ?  (<Component {... props}/>) : path === "/joined/:class"?  
+                ( <Redirect to={`/login?path=${location.pathname}`} />) :
+                (<Redirect to={`/private?path=${location.pathname}`} />)
+            }
+        />
+    )
+
+}
+export default withRouter(MemberRoute);
